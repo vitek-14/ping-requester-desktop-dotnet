c724b30233ce8607e9567e650e3e16f6fa9875ca
vitek-14 <vitekkt@gmail.com>
2025-08-21 22:31:25 +0200

    MainForm renamed

--- Ping_Requester/MainForm.Designer.cs
+++ Ping_Requester/MainForm.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Ping_Requester
 {
-    partial class Form1
+    partial class MainForm
     {
         /// <summary>
         ///  Required designer variable.
@@ -28,10 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            SuspendLayout();
+            // 
+            // MainForm
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(811, 485);
+            ForeColor = SystemColors.ActiveCaption;
+            Name = "MainForm";
+            Text = "Form1";
+            ResumeLayout(false);
         }
 
         #endregion
