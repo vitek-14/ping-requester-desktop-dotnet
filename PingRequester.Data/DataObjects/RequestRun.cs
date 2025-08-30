a71d68f4574a5a711b019378a06014dbbf2812c6
vitek-14 <vitekkt@gmail.com>
2025-08-30 21:21:17 +0200

    RequestRun expanded by more properties

--- PingRequester.Data/DataObjects/RequestRun.cs
+++ PingRequester.Data/DataObjects/RequestRun.cs
@@ -11,13 +11,20 @@ namespace PingRequester.Data.DataObjects
 {
     public class RequestRun : DataObject
     {
-        private Requester requester;
-        private string ipAdress;
+        private string hostname;
+        private string ipv4;
+        private int packetsSent;
+        private int packetsRecieved;
+        private int packetsLost;
+        private DateTime start;
+        private DateTime end;
+        private int maxTime;
+        private int minTime;
+        private float averageTime;
         private Queue<Packet> packets;
 
-        public RequestRun(Requester requester)
+        public RequestRun()
         {
-            this.requester = requester;
             Packets = new Queue<Packet>();
         }
 
@@ -25,6 +32,66 @@ namespace PingRequester.Data.DataObjects
         {
             get => packets;
             set => SetProperty(ref packets, value);
+        }
+
+        public string Hostname
+        {
+            get => hostname;
+            set => SetProperty(ref hostname, value);
+        }
+
+        public string Ipv4
+        {
+            get => ipv4;
+            set => SetProperty(ref ipv4, value);
+        }
+
+        public int PacketsSent 
+        { 
+            get => packetsSent;
+            set => SetProperty(ref packetsSent, value);
+        }
+
+        public int PacketsRecieved
+        { 
+            get => packetsRecieved;
+            set => SetProperty(ref packetsRecieved, value);
+        }
+
+        public int PacketsLost
+        {
+            get => packetsLost;
+            set => SetProperty(ref packetsLost, value);
+        }
+
+        public DateTime Start
+        {
+            get => start;
+            set => SetProperty(ref start, value);
+        }
+
+        public DateTime End
+        {
+            get => end;
+            set => SetProperty(ref end, value);
+        }
+
+        public int MaxTime
+        {
+            get => maxTime;
+            set => SetProperty(ref maxTime, value);
+        }
+
+        public int MinTime
+        {
+            get => minTime;
+            set => SetProperty(ref minTime, value);
+        }
+
+        public float AverageTime
+        {
+            get => averageTime;
+            set => SetProperty(ref averageTime, value);
         }
     }
 }
