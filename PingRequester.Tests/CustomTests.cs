53c563c9d2310b3ad9dda04d4275d9ddb3c5ea1c
vitek-14 <vitekkt@gmail.com>
2025-09-15 09:36:49 +0200

    test project added, first custom test added

---
+++ PingRequester.Tests/CustomTests.cs
@@ -0,0 +1,25 @@
+﻿using Xunit;
+using PingRequester.BusinessLayer;
+
+using PingRequester.Data.DataObjects;
+
+namespace PingRequester.Tests
+{
+    public class CustomTests
+    {
+        public CustomTests()
+        {
+        }
+
+        [Fact]
+        public void GetJsonSettingsContentTest()
+        {
+            // Arrange
+            var jsonSettingsService = new JsonService<Settings>("config/SettingsConfig.json");
+            var settings = jsonSettingsService.LoadFileContent();
+
+            // Assert
+            Assert.NotNull(settings);
+        }
+    }
+}
