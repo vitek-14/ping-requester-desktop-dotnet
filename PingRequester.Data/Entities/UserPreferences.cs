8bfb4f6c0dbfea4990787d3bf712a507df181d97
Vít Šilhavý <vitekkt@gmail.com>
2026-03-08 20:34:24 +0100

    entities (basic structure added) added

---
+++ PingRequester.Data/Entities/UserPreferences.cs
@@ -0,0 +1,14 @@
+﻿using PingRequester.Data.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PingRequester.Data.Entities
+{
+    public class UserPreferences : Entity
+    {
+        // TODO: complete props
+    }
+}
