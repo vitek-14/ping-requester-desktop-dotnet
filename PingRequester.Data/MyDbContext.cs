b8c079e02545ec5176dab19865c1182406bc9504
Vít Šilhavý <vitekkt@gmail.com>
2026-03-09 23:38:20 +0100

    MyDbContext added

---
+++ PingRequester.Data/MyDbContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PingRequester.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PingRequester.Data
+{
+    public class MyDbContext : DbContext
+    {
+        public DbSet<RequestRunSession> Sessions { get; set; }
+        public DbSet<UserPreferences> Preferences { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            string dbPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "ping_requester.db");
+            optionsBuilder.UseSqlite($"Data Source={dbPath}");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<UserPreferences>()
+                .HasMany(u => u.Sessions)
+                .WithOne(s => s.UserPreferences)
+                .HasForeignKey(s => s.UserPreferencesId);
+        }
+    }
+}
