4279a7f2c63a910a4399ed75f4b048c7e5716f11
Vít Šilhavý <vitekkt@gmail.com>
2026-03-10 10:34:25 +0100

    IEntityService and SessionService added

---
+++ PingRequester.BusinessLayer/IEntityService.cs
@@ -0,0 +1,19 @@
+﻿using PingRequester.Data.Base;
+using PingRequester.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PingRequester.BusinessLayer
+{
+    internal interface IEntityService
+    {
+        public Entity? GetById(int id);
+        public IEnumerable<Entity> GetAll();
+        public bool Add(Entity entity);
+        public bool Update(Entity entity);
+        public bool Delete(Entity entity);
+    }
+}
---
+++ PingRequester.BusinessLayer/SessionService.cs
@@ -0,0 +1,77 @@
+﻿using PingRequester.Data;
+using PingRequester.Data.Base;
+using PingRequester.Data.Entities;
+
+namespace PingRequester.BusinessLayer
+{
+    public class SessionService : IEntityService
+    {
+        private readonly MyDbContext _context;
+
+        public SessionService(MyDbContext context)
+        {
+            _context = context;
+        }
+
+        public Entity? GetById(int id)
+        {
+            return _context.Sessions.FirstOrDefault(s => s.Id == id);
+        }
+        public IEnumerable<Entity> GetAll()
+        {
+            return _context.Sessions.ToList();
+        }
+
+        public bool Add(Entity entity)
+        {
+            if (entity == null)
+            {
+                return false;
+            }
+
+            try
+            {
+                _context.Sessions.Add((RequestRunSession)entity);
+                _context.SaveChanges();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error occured while adding entity to the database. Error type: {ex}; message: {ex.Message}");
+                return false;
+            }
+        }
+        public bool Update(Entity entity)
+        {
+            if (entity == null)
+            {
+                return false;
+            }
+
+            _context.Sessions.Update((RequestRunSession)entity);
+            _context.SaveChanges();
+
+            return true;
+        }
+
+        public bool Delete(Entity entity)
+        {
+            if (entity == null)
+            {
+                return false;
+            }
+
+            try
+            {
+                _context.Sessions.Remove((RequestRunSession)entity);
+                _context.SaveChanges();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error occured while deleting entity from the database. Error type: {ex}; message: {ex.Message}");
+                return false;
+            }
+        }
+    }
+}
