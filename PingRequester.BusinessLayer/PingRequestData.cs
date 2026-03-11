182f401c08e6fb8a4c06efdf1b9fe52889078d07
Vít Šilhavý <vitekkt@gmail.com>
2026-03-11 20:58:20 +0100

    PingRequestData facade added
    this facade is merging Preferences and Sessions services

---
+++ PingRequester.BusinessLayer/PingRequestData.cs
@@ -0,0 +1,22 @@
+﻿using PingRequester.BusinessLayer.Services;
+using PingRequester.Data;
+
+namespace PingRequester.BusinessLayer
+{
+    public class PingRequestData
+    {
+        private SessionService session;
+        private PreferencesService preferences;
+
+        public PingRequestData()
+        {
+            session = new SessionService(CreateContext);
+            preferences = new PreferencesService(CreateContext);
+        }
+
+        public SessionService Sessions { get => session; }
+        public PreferencesService Preferences { get => preferences; }
+
+        private MyDbContext CreateContext() => new MyDbContext();
+    }
+}
